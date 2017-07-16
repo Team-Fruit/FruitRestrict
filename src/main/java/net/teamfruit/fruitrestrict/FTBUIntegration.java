9560a735fd253a6ce142a5ba76b1612ce66e14b5
sjcl <fruit@bebehp.com>
2017-07-16 14:33:50 +0900

    FTBUIntegration

---
+++ src/main/java/net/teamfruit/fruitrestrict/FTBUIntegration.java
@@ -0,0 +1,19 @@
+package net.teamfruit.fruitrestrict;
+
+import com.feed_the_beast.ftbu.api.FTBUtilitiesAPI;
+import com.feed_the_beast.ftbu.api.FTBUtilitiesPlugin;
+import com.feed_the_beast.ftbu.api.IFTBUtilitiesPlugin;
+
+import net.minecraftforge.common.MinecraftForge;
+
+public enum FTBUIntegration implements IFTBUtilitiesPlugin {
+	@FTBUtilitiesPlugin INSTANCE;
+
+	public static FTBUtilitiesAPI API;
+
+	@Override
+	public void init(final FTBUtilitiesAPI api) {
+		API = api;
+		MinecraftForge.EVENT_BUS.register(INSTANCE);
+	}
+}
